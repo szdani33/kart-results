a05be1f6407dcfdd05ad7157449facef821dffc0
Daniel Szabo <daniel_szabo@epam.com>
2018-10-11 14:12:45 +0200

    app base

---
+++ src/main/java/hu/daniels/kartresults/KartResultsApplication.java
@@ -0,0 +1,12 @@
+package hu.daniels.kartresults;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KartResultsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KartResultsApplication.class, args);
+	}
+}
